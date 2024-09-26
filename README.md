4ec6f00fafab29f56659e99d83dd9dc1fd60ec8b
rpatroni <rafael.patronilo@hotmail.com>
2024-09-26 16:25:00 +0100

    Some crawling code

---
+++ README.md
@@ -0,0 +1,3 @@
+# WARNING - SECURITY VULNERABILITIES
+
+This repository is a cybersecurity experiment with **deliberate security vulnerabilities**
---
+++ index.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('database.sqlite');
+
+const url = 'http://localhost:8080/posts';
+
+
+async function main(){
+    const response = await fetch(url)
+    const posts = await response.json();
+    for(let post of posts) {
+        const { title, content } = post;
+        db.exec(`INSERT INTO posts (title, content) VALUES ("${title}", "${content}")`);
+    }
+}
+
+main();
