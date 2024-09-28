7cb493eebc8c646ccb96303813af0b776cec90ab
rpatroni <rafael.patronilo@hotmail.com>
2024-09-28 12:20:21 +0100

    Switch to xss

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch');
-const sqlite3 = require('sqlite3');
+//const sqlite3 = require('sqlite3');
+const ejs = require('ejs');
 
-const db = new sqlite3.Database('database.sqlite');
+//const db = new sqlite3.Database('database.sqlite');
 
 const url = 'http://localhost:8080/posts';
 
@@ -9,10 +10,20 @@ const url = 'http://localhost:8080/posts';
 async function main(){
     const response = await fetch(url);
     const posts = await response.json();
-    for(let post of posts) {
-        const { title, content } = post;
-        db.exec(`INSERT INTO posts (title, content) VALUES ("${title}", "${content}")`);
-    }
+    // posts = [
+    //     { title: 'Post 1', content: 'Give me your password >:)</p> <script type="text/javascript">console.log("Very dangerous javascript")</script><p>' },
+    // ]
+    console.log(ejs.render(`
+        <html>
+        <head><title>Post</title></head>
+        <body>
+            <% for (post of posts) {%>
+                <h1><%- post.title %></h1>
+                <p><%- post.content %></p>
+            <% } %>
+        </body>
+        </html>
+        `, {posts:posts}));
 }
 
 main();
