87423c9176f4ade327a8deb0147a224366628b67
rpatroni <rafael.patronilo@hotmail.com>
2024-09-29 16:56:40 +0100

    rm ctx

--- vuln_cmi.js
+++ vuln_cmi.js
@@ -2,26 +2,18 @@ const fs = require('fs');
 const child_process = require('child_process');
 const os = require('os');
 
-function savePost(context){
-    let dest = `posts/${context.post.title}`;
-    fs.writeFileSync(dest, context.post.content);
-    if (context.post["date"] !== undefined){
-        child_process.exec(`touch -t ` + context.post.date + ` "${dest}"`);
+function savePost(post){
+    let dest = `posts/${post.title}`;
+    fs.writeFileSync(dest, post.content);
+    if (post["date"] !== undefined){
+        child_process.exec(`touch -t ` + post.date + ` "${dest}"`);
     }
-}
-
-function buildContext(index, post){
-    let context = {index: index, post: post};
-    return context;
 }
 
 function savePosts(posts){
     fs.mkdirSync('posts', {recursive: true});
-    let i = 0;
     for (let post of posts){
-        let context = buildContext(i, post);
-        savePost(context);
-        i++;
+        savePost(post);
     }
 }
 
